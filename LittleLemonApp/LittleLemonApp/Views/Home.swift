50e79a572d14d4e44a80f3f9d1c6d2d1acfb1e58
dilip210197 <dilip210197@gmail.com>
2024-03-09 08:11:49 +0530

    Capstone Project

---
+++ LittleLemonApp/LittleLemonApp/Views/Home.swift
@@ -0,0 +1,22 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+//  Created by Dilip Kumar on 08.03.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
